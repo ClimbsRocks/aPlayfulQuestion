717d1bb22ae3d026f4510855437d54cac4ddfca3
ClimbsRocks <climbsbytes@gmail.com>
2016-03-09 12:12:55 -0800

    adds questions dreamt up during plane flight

---
+++ questions.js
@@ -0,0 +1,203 @@
+// This is designed as my favorite type of mental playground- giving you just enough structure to use as a jumping off point for making your own adventures. Make this your own- add your own flair to these questions, your own constraints and stipulations and ridiculous twists (my standby is to add "with an incredible headpiece" to the end of every imagined scenario). Try asking these questions in different contexts. Ask them first for a family member, then a friend, then a lover (favorite trip with a family member, favorite trip with a friend, favorite trip with a lover). Explore and laugh and play together. The best use of this game is not to rush through and answer all the questions in a fact-finding mission, but rather to have found a single question that sparks hours and hours of story-telling and memories and playful imagination of worlds created together. 
+
+// Many of these questions are intentionally vague- part of the fun is to see how you choose to interpret it. 
+
+// Oh, and some of these get super personal. Feel free to skip over them, or if you're daring enough to venture into that territory, do so with empathy and understanding to give your partner the support they need to delve into those memories. 
+
+// There be dragons, and we're going to ride them to your favorite place in the woods, while wearing an awesome feathered hat. 
+
+// make them click- Yes, I'm game to make my own adventures from these questions!
+
+{
+  'thingsThatActuallyHappend': [
+    'When was the last time you felt yourself right on the edge of something big, not yet knowing if it would work out or not?', 
+    "What's the least you've ever packed for a trip longer than 2 days?",
+    "What's the most you've ever packed for a trip?",
+    "What's the most unprepared you've ever been for a trip?",
+    "What's one of the moments you've been most unprepared for a presentation?",
+    "What's one of the worst presentations/talks you've ever given?",
+    "What's one of the best presentations/talks you've given?",
+    "What's the most nervous you can recall being before a big presentation?",
+    "What's the most nervous you can recall being, ever?",
+    "What's one of your most embarrassing moments?",
+    "What's a memory that music plays a huge role in?",
+    "Tell a story of one of your more memorable concerts",
+    "What does a summer night mean to you?",
+    "What's a favorite bench, and the stories to go with it?",
+    "What's a favorite memory of being in a car?",
+    "What's a favorite memory of being in a car with music?",
+    "What's a favorite memory of being in a car with music with the windows down?",
+    "Spin me a story of a time you got lost and you loved it"
+    "Tell me a tale of a time you got lost and it ended up being a sketchy situation"
+    "What's a movie you've always gotten crap for not having seen?"
+    "Scariest plane ride you can recall?"
+    "Worst experience in an airplane bathroom?"
+    "Best stranger you've met on an airplane"
+    "Most memorable stranger you've met while traveling?"
+    "Best outdoor guide (rafting, fishing, climbing, kayaking, horseback riding, 4x4ing, etc.)"
+    "Best time you got to be somebody's guide for an outdoor activity"
+    "Best time you got to be a tour guide for someone in a city you don't live in"
+    "Favorite trip you've organized for a group of friends"
+    "What are you proudest of in your work so far?"
+    "What are you proudest of in your platonic relationships so far?"
+    "What are you proudest of in your romantic relationships so far?"
+    "What's the most ridiculous outfit you've worn to work?"
+    "What's the most fun outfit you can remember wearing?"
+    "What was an outfit you expected to feel ridiculous, but you actually ended up loving?"
+    "What's the most memorable live music you remember being played in an unexpected setting? That Jazz trumpeter in the abandoned alleyways of the FiDi late at night, just to hear the world ring with the notes of his play and sadness, or stumbling upon Harry Townsley meditating on the piano in the basement of the dorm after a particularly saddening fight with a lover?"
+    "I promise this will get rapidly more interesting, but we'll start with 'What did you study in school?'. Imagine you're 17 again. What would you choose to study now?"
+    "What is your favorite memory tied to the air guitar?"
+    "Karaoke."
+    "Some of the most evocative imagery you can recall, whether from a song or movie or book or memory"
+    "Most feeling you've gotten from words someone's written for you"
+    "Most ridiculous poem/thing you've written for a lover"
+    "2nd best memory of Type 2 fun (the type of fun that is not fun at the time, but is fun to recall afterwards and makes you want to do more- mountaineering being the prime example)"
+    "Most your abs have hurt from laughing"
+    "Most scared you've ever been on a plane"
+    "Any emergencies that have ever happened on a plane you're on"
+    "A couple of times you've called 911"
+    "Most memorable time going through airport security"
+    "Most expensive phone call you've placed"
+    "Biggest deal you've ever signed"
+    "Most expensive contract you've ever signed (buying a house, leasing a car, working for company A instead of company B, etc.)"
+    "Biggest missed opportunity one of your friends has had"
+    "Biggest missed opportunity you've had that you don't regret at all"
+    "Something that everybody else thinks you should probably regret but you totally don't"
+    "Something you will never ever ever ever- seriously EVER do ever again for the rest of your life. Ever."
+    "Longest plane ride you've taken"
+    "Cushiest travel from point A to point B"
+    "Most bare-bones and likely sketchy, I'm-not-sure-I-would-recommend-that-to-a-friend journey you've made"
+    "Strangest place you've slept"
+    "Longest journey you've taken not in a plane or car"
+    "How connected do you feel to your ancestors/forebears/predecessors/family tree/people on your ancestral shrine?"
+    "What is the most scared for your life you can remember being?"
+    "What is the closest to death you're aware of coming?"
+    "Most memorable breakfast on an adventure day?"
+    "Coldest night you've ever slept in?"
+    "Best night's sleep you've had"
+    "Longest you've stayed up continuously?"
+    "Most tired you've ever been after an athletic endeavor?"
+    "Happiest you've ever been from a physical performance? Climbed a really hard route, or just managed to swim to shore from a perilous bouy as the storm rolled in"
+    "Best time you ran into a friend totally unexpectedly"
+    "Most interesting bathroom (or better yet, lack thereof) you've used"
+    "Time you've most needed to use the restroom and been forced to wait..."
+    "Time you've felt most out of place"
+    "Time you've very clearly been in the minority- defined however you would like"
+    "Time you felt most sexy"
+    "Time you felt least sexy, but somebody else did (can be either creepy or endearing or anything else in between)"
+    "Time you felt strongest"
+    "Time you felt weakest"
+    "Favorite alleyway in the world"
+    "Tiniest airport you've ever flown into"
+    "Time you were in a foreign land and realized you'd just violated local customs in a big way."
+    "Tallest heels you've ever worn"
+    "Tallest height you've ever stood on"
+    "Deepest you've ever been underwater"
+    "Weirdest or most memorable thing you've seen in the water"
+    "Most dangerous weather you've had to deal with"
+    "Most scared you've been of the weather"
+    "Most unexpected place you've ever danced"
+    "Biggest fire you've been involved with"
+    "Best campfire"
+    "Scaredest night sleeping, or failing to sleep"
+    "Song you refuse to listen to"
+    "Albums/artists/songs that are closely tied to specific memories or time periods"
+    "Favorite pair of shoes or boots"
+    "Most worn thing you've ever owned"
+    "Most gorgeous sunset"
+    "Most striking time you've looked up at the night sky"
+    "Most desolate place you've ever been/ most removed from society/most unreachable"
+    "Most disconnected from your normal life you've been"
+    "The time that artwork hit you right in the chest and knocked you unsteady on your feet"
+    "Most memorable museum"
+    "Most memorable exhibition"
+    "Weirdest museum you've seen"
+    "Favorite weird tourist trap (Biggest Ball Of Yarn In The World!)"
+    "A time you've been kicked out of somewhere"
+    "An unexpected interaction you've had with the police"
+    "A totally benign and innocent thing you've done that others have not interpreted that way"
+    "Smelliest thing you can recall"
+    "The largest number of animals you can remember seeing at once"
+    "Tallest mountain/building/pedestal you've ever stood on"
+    "Highest (altitude wise) you've ever felt"
+    "That moment everything seemed to be going so well and you got to bask in life feeling so great."
+    "That song you have listened to on repeat a few too many times to be embarrassed about any more."
+    "Favorite form of meditation"
+    "Memorable moment meditating"
+    "That song or movie or tv show you know every word to, even if it's not necessarily your favorite"
+    "Worst car accident you've been in."
+    "Time your car broke down the worst"
+    ""
+  ],
+  'ridiculousSituations': ["Otherwise known as 'what would you do if...'",
+    "Who would you most like to share a winning lottery ticket with? \n Ok, let's make this more interesting: Same lottery ticket, same person you're sharing it with. Imagine what you would do if it's for $5. Keep going up by two orders of magnitude each time: $5, $500, $50,000, $5 million, $500 million, and finally, $5 billion.",
+    "What fictional or actual villian would you most like to be?",
+    "What real or fictional sidekick would you most like to be?",
+    "You must live the life of somebody who spends a lot of time suffering. Who is it? Can be any kind of suffering (Mandela, a normal woman living in nearly any society pre-1920, any athlete who spends hours and hours every day suffering for performance gains, etc.",
+    "What would happen if you won the lottery, and offered to build your family (mom/dad/brothers/sisters) a big family compound?"
+    "Would you go live on Mars? How does that answer change if it is or is not a one-way ticket?"
+    "Would you live on a spaceship bound for Alpha Centauri, knowing it will take 8 generations to reach that distant star?"
+    "You are transported back in time to any time/place of your choosing, and you are allowed to take 3 items with you. Assume that language is not a barrier."
+    "Who (living or dead, real or fictional) would you be personal assistant to for a year?"
+    "If you could change history in just one way, what would you do?"
+    "Imagine you're dropped in the middle of an ocean. You are not allowed to contact the outside world in any way. But you are allowed to bring absolutely anything with you that you would like (other than communication or propulsion devices)."
+    "Imagine you're dropped on an abandoned island, and told you're not allowed to leave for 26 years. But you're allowed to invite along anybody you'd like (they don't have to accept), and you're allowed to initially stock the island with any supplies/materials you'd like."
+    "Imagine that an evil witch came along and deleted one item from your life, and said you could never have any other version of that ever again the future (no smartphone or whatever they evolve into, no bed, no toothbrush). What would be the worst thing to lose?"
+    "Biggest thing you've ever made with your hands (and tools)"
+    "Coolest repair you've made to a physical thing"
+    "A time you helped somebody who was in physical distress- whether broken bone or just in an unsafe area"
+    "Best barbecue you've had"
+    "Food that you felt weirdest about eating at the time, even if your tastes have changed and you like it now"
+    "Strangest animal that you know of"
+    "A memorable photo you took"
+    "Closest you've come to a wild animal attack"
+    "Most peaceful night you've spent outside"
+    "Happiest you've ever been to sink into a warm shower or bath"
+    "Most unexpected physical challenge you've taken on"
+    "Strangest sign you've seen beside the road."
+  ],
+  'imaginedFutures': [],
+  'imaginativeGiftCertificates': [
+    // Note, all of these are prefaced by "You are given a gift certificate for...". Please place your own constraints on the problem :)
+    "You are given a gift certicate for one custom-made statue of any shape/size/material/subject. What is it, and where do you place it?",
+    "One pet. Real or imagined. No practical constraints (for example, your landlord is totally cool with you housing a hippogriff in your tiny studio, and you don't need to worry about coming home to walk it or anything'"
+
+  ],
+  'fearsAndHopesAndDreamsAndPhilosophies': [
+    "What are you most scared of?"
+    "What's a seemingly minor thing you're unreasonably frightened by?"
+    "How do you answer the question of 'What\'s the meaning of life'?"
+    "What does your life 10 years from now look like? \n (Answer this before moving on!!) \n What does your life 10 years past look like? \n Scientists did an interesting study showing that while most of us expect our life circumstances to have changed in 10 years time, we expect to be generally the same people traveling in the same direction we are right now. Yet, when we're asked to look 10 years back, we almost inevitably tell tales of great change and transformation. There's a wonderful dissonance to explore there."
+    "What's one of the biggest moments of dissonance you've experienced in your own life? These are moments like 'I identify as being the type of person who travels a lot, and yet, I haven\'t traveled in years'"
+    "Skip over this one unless you're ready to explore something extremely personal: \nHow has death affected your life?"
+    "From the book strangers drowning- you're faced with a choice between saving two drowning strangers, or a single person very close to you."
+    "There are millions of things severely broken with our world (children dying of hunger, diseases running rampant, systematic repression of groups of people, and homeless strangers in your own city). How do you deal with this?"
+    "What do you most desire to contribute to this world?"
+    "What are some long-term goals you've had that you've accomplished?"
+    "What's a promise you've made that you hold dear to this day?"
+    "What's a promise you've made and broken?"
+    "If you could do it all over again, what would you do differently?"
+    "Which do you fear more- wishing you had not done something, or wishing that you had?"
+    ""
+  ],
+  'friends': [
+    "Who's that one friend you trust with all your philosophical wanderings, your emotional wanderings, your intellectual explorations?"
+    "Which friend do you most enjoy hiking with, and why?"
+    "Which friend do you most enjoy crashing with?"
+    "Who's a friend that you're close with because you've been through so many years together, but that if you met them now for the first time you'd never start a friendship with them?"
+    "Which friend is most fun to start a dance floor with?"
+    "Who would you most like to be strapped into a tandem bike with?"
+    "Who would you most like to be strapped into a parachute with? How does that answer change if you know that the main chute is going to fail?"
+    "What friend do you have the least in common with?"
+    "What friend has seen you through the roughest part of your life?"
+    "What friend is always proposing crazy adventures?"
+    "What friend is not the one proposing the crazy adventures, but is always totally game to go on them and make them even more fun?"
+    "What friend so reliably makes an event/trip fun that you would do anything if you knew they were going to be there?"
+    "What friend would you most want to go to a karaoke bar with?"
+    "What friend do you most enjoy starting a dance floor with?"
+    "Which of your friends gets put next to crazy Aunt Marge at your wedding?"
+    "Who would you most like to build civilization on a new planet with?"
+  ]
+
+}
+
